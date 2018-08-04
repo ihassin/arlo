ecd9a669c527d1875481f2dfcde01709f560b2ff
Itamar Hassin <ihassin@mac.com>
2018-08-03 20:40:16 -0400

    Added get_device_info method

--- lib/arlo/devices.rb
+++ lib/arlo/devices.rb
@@ -4,5 +4,10 @@ module Arlo
       profile = get('https://arlo.netgear.com/hmsweb/users/devices', token)
       JSON.parse(profile.body)
     end
+
+    def get_device_info device_name, device_list
+      raise 'Missing device_name' unless device_name
+      device_list['data'].select {|device| device['deviceName'] == device_name }[0]
+    end
   end
 end
--- spec/arlo_gem_spec.rb
+++ spec/arlo_gem_spec.rb
@@ -32,6 +32,18 @@ RSpec.describe 'Arlo Gem' do
         expect(devices['data']).not_to be nil
       end
     end
+
+    context 'device operations' do
+      it 'gets a device by name' do
+        device_name = ENV['ARLO_TEST_DEVICE']
+        raise 'Missing ARLO_TEST_DEVICE environment variable' unless device_name
+        VCR.use_cassette 'devices' do
+          devices = @api.get_devices @token
+          device = @api.get_device_info device_name, devices
+          expect(device['deviceName']).to eq device_name
+        end
+      end
+    end
   end
 
 end
